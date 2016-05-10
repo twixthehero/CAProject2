0dde1dada933ea6e086f2738c7acff930bbb662c
Maximilian Wright <twixthehero@gmail.com>
2016-05-10 16:58:24 -0400

    Added menu functionality for spots

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var g_renderer = new PIXI.WebGLRenderer(window.width, window.height);
+let g_renderer = new PIXI.WebGLRenderer(window.width, window.height);
 g_renderer.view.style.display = "block";
-var g_container = new PIXI.Container();
+let g_container = new PIXI.Container();
 g_container.hitArea = new PIXI.Rectangle(0, 0, g_renderer.width, g_renderer.height);
 g_container.interactive = true;
 g_container.buttonMode = true;
@@ -61,3 +61,72 @@ var platter = new Platter();
 platter.init();
 
 requestAnimationFrame(update);
+
+window.onload = init;
+
+var g_guiContainer = undefined;
+var gui = new dat.GUI({autoPlace: false});
+var MIN_TEMPO = 60;
+var MAX_TEMPO = 180;
+var MIN_PITCH = 27.5;
+var MAX_PITCH = 4186;
+var TEMPO_RANGE;
+var PITCH_RANGE;
+
+function init()
+{
+    g_guiContainer = document.getElementById("guicont");
+
+    let settings = {
+        minTempo: MIN_TEMPO,
+        maxTempo: MAX_TEMPO,
+        minPitch: MIN_PITCH,
+        maxPitch: MAX_PITCH
+    };
+
+    var minTCont = gui.add(settings, "minTempo", 20, 120);
+    var maxTCont = gui.add(settings, "maxTempo", 120, 240);
+    var minPCont = gui.add(settings, "minPitch", 27.5, 2000);
+    var maxPCont = gui.add(settings, "maxPitch", 2000, 4186);
+
+    minTCont.onChange(function(value)
+    {
+        MIN_TEMPO = value;
+        calcRanges();
+    });
+    maxTCont.onChange(function(value)
+    {
+        MAX_TEMPO = value;
+        calcRanges();
+    });
+    minPCont.onChange(function(value)
+    {
+        MIN_PITCH = value;
+        calcRanges();
+    });
+    maxPCont.onChange(function(value)
+    {
+        MAX_PITCH = value;
+        calcRanges();
+    });
+
+    addGui(gui);
+
+    calcRanges();
+}
+
+function calcRanges()
+{
+    TEMPO_RANGE = MAX_TEMPO - MIN_TEMPO;
+    PITCH_RANGE = MAX_PITCH - MIN_PITCH;
+}
+
+function addGui(gui)
+{
+    g_guiContainer.appendChild(gui.domElement);
+}
+
+function removeGui(gui)
+{
+    g_guiContainer.removeChild(gui.domElement);
+}
--- js/platter.js
+++ js/platter.js
@@ -11,30 +11,26 @@ function Platter()
 
     this.update = function()
     {
-        for (var i = 0; i < this.spots.length; i++)
-        {
+        for (let i = 0; i < this.spots.length; i++)
             this.spots[i].update();
-        }
     };
 
     this.render = function()
     {
         this.g.clear();
 
-        for (var i = 0; i < this.spots.length; i++)
+        for (let i = 0; i < this.spots.length; i++)
             this.spots[i].render(this.g);
     };
 
     this.onMouseDown = function(pos)
     {
-        for (var i = 0; i < this.spots.length; i++)
-        {
+        for (let i = 0; i < this.spots.length; i++)
             if (this.spots[i].contains(pos.x, pos.y))
             {
                 this.index = i;
                 break;
             }
-        }
     };
 
     this.onMouseUp = function(pos)
@@ -42,16 +38,23 @@ function Platter()
         if (this.index == -1)
             this.createSpot(pos.x, pos.y);
         else
-            this.index = -1;
+        {
+            if (this.spots[this.index].contains(pos.x, pos.y))
+                this.spots[this.index].onClick();
+            else
+            {
+                for (let i = 0; i < this.spots.length; i++)
+                    this.spots[i].offClick();
+
+                this.index = -1;
+            }
+        }
     };
 
     this.onMouseDrag = function()
     {
         if (this.index != -1)
-        {
-            //console.log(g_mouseDelta);
             this.spots[this.index].move(g_mouseDelta);
-        }
     };
 
     this.createSpot = function(x, y)
@@ -62,6 +65,32 @@ function Platter()
         spot.position.y = y;
 
         this.spots.push(spot);
+    };
+}
+
+function Controls(pat, rad, vol, col)
+{
+    this.pattern = pat;
+    this.radius = rad;
+    this.volume = vol;
+    this.color = col;
+    this.mute = false;
+
+    this.gui = new dat.GUI({autoPlace: false});
+    this.gui.add(this, "pattern");
+    this.gui.addColor(this, "color");
+    this.gui.add(this, "radius", 3, 20);
+    this.gui.add(this, "volume", -30, 0);
+    this.gui.add(this, "mute");
+
+    this.add = function()
+    {
+        addGui(this.gui);
+    };
+
+    this.remove = function()
+    {
+        removeGui(this.gui);
     };
 }
 
@@ -82,34 +111,78 @@ function Spot()
         };
         this.radius = rad;
         this.pattern = "";
+        this.color = 0x0000FF;
 
         this.osc = new Tone.Oscillator(440, "sawtooth").toMaster();
         this.osc.volume.value = -15;
+
+        this.tempo = this.calcTempo();
+        this.pitch = this.calcPitch();
+
+        this.controls = new Controls(this.pattern, this.radius, this.osc.volume.value, this.color);
+
+        this.enabled = true;
+        this.osc.start();
+    };
+
+    this.calcTempo = function()
+    {
+        this.tempo = MIN_TEMPO + TEMPO_RANGE * (this.position.x / g_renderer.width);
+    };
+
+    this.calcPitch = function()
+    {
+        this.pitch = MIN_PITCH + PITCH_RANGE * ((g_renderer.height - this.position.y) / g_renderer.height);
     };
 
     this.move = function(delta)
     {
         this.position.x += delta.x;
         this.position.y += delta.y;
+
+        if (this.position.x < 0) this.position.x = 0;
+        if (this.position.x >= g_renderer.width) this.position.x = g_renderer.width - 1;
+        if (this.position.y < 0) this.position.y = 0;
+        if (this.position.y >= g_renderer.height) this.position.y = g_renderer.height - 1;
     };
 
-    this.update = function() {
+    this.update = function()
+    {
+        this.pattern = this.controls.pattern;
+        this.radius = this.controls.radius;
+        this.osc.volume.value = this.controls.volume;
+        this.color = this.controls.color;
 
+        if (this.osc.state == "started" && this.controls.mute)
+            this.osc.stop();
+        else if (this.osc.state == "stopped" && !this.controls.mute)
+            this.osc.start();
+
+        this.calcTempo();
+        this.calcPitch();
+
+        this.osc.frequency.value = this.pitch;
     };
 
     this.render = function(g)
     {
-        g.beginFill(0x2222FF, 1.0);
+        g.beginFill(this.color, 1.0);
         g.drawCircle(this.position.x, this.position.y, this.radius);
         g.endFill();
     };
 
-    this.onClick = function() {
+    this.onClick = function()
+    {
+        this.controls.add();
+    };
 
+    this.offClick = function()
+    {
+        this.controls.remove();
     };
 
     this.contains = function(xpos, ypos)
     {
-        return Math.sqrt(Math.pow(xpos - this.position.x, 2) + Math.pow(ypos - this.position.y, 2)) < this.radius;
+        return Math.pow(xpos - this.position.x, 2) + Math.pow(ypos - this.position.y, 2) < this.radius * this.radius;
     };
 }
